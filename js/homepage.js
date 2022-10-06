fab5b99151934e043999b4be12abbda4b0ad952a
kwwwk <91783866+kwwwk@users.noreply.github.com>
2022-10-06 22:09:26 +0800

    Displayed LJ created, skills & courses dynamically

--- js/homepage.js
+++ js/homepage.js
@@ -1,30 +1,76 @@
 const homepage = Vue.createApp({
     data(){
         return {
-            learning_journey:[],
+            AllRoles_dict: [],
+            Allcourses_dict: [],
         }
     },
     mounted:function()  {
-            console.log("are we supposed to host on web!!!! (run on wamp)")
             url="../db/getLearningJourney.php"
-            // const data = { LJ_ID: LJ_ID, Staff_ID: Staff_ID,
-            //     LJRole_Name: LJRole_Name, LJRole_Description: LJRole_Description, Department:Department,Key_Task:Key_Task, LJRole_img:LJRole_img, Skill_Name:Skill_Name, Type_of_Skills:Type_of_Skills, Level_of_Competencies:Level_of_Competencies, Skill_img:Skill_img,Course_ID:Course_ID , Course_Name:Course_Name, Course_Desc:Course_Desc , Course_Type:Course_Type, Course_Category:Course_Category
-            //     }
-            var Staff_ID = 00001
+            var Staff_ID = 00000
             const data ={Staff_ID:Staff_ID}
 
             axios.get(url, {
                 params: data
             })
             .then(response => { 
-                console.log(response.data)
-                this.learning_journey= response.data
+                var LJdata = response.data
                 this.status = response.data
+                this.getLJdetails(LJdata)
+                this.getCourseDetails(LJdata);
             })
             .catch(error => {
                 this.status = 'There was an error: ' + error.message 
             }) 
             
+        },
+    methods:{
+        getLJdetails(LJdata){
+            var tempRoleDict = {};
+            var tempSkillDict = {};
+            for (i=0; i<LJdata.length; i++){
+                var LJid = LJdata[i].LJ_ID; //a staff can hv zero-many LJ
+                var LJroleName = LJdata[i].LJRole_Name;
+                var SkillId = LJdata[i].Skill_ID; //a LJ can have zero-many skills
+                var SkillName = LJdata[i].Skill_Name;
+
+                //if it is a new LJ
+                if(!tempRoleDict[LJid]){
+                    var tempSkillDict ={};
+
+                    //if it is a new skills under that LJ
+                    if(! tempSkillDict[SkillId] ){
+                        tempSkillDict[SkillId]= {SkillId:SkillId, SkillName: SkillName}
+                    }
+                    tempRoleDict[LJid] ={role:LJroleName, skill:tempSkillDict}
+                    this.AllRoles_dict.push({LJid: LJid, LJrole:LJroleName, skill:tempSkillDict})
+                }
+
+                //if LJ already exist but another skills & course
+                else{
+                    if(! tempSkillDict[SkillId] ){
+                        tempSkillDict[SkillId]= {SkillId:SkillId, SkillName: SkillName}                    }
+                }
+            }
+            return this.AllRoles_dict
+        },
+        getCourseDetails(LJdata){
+            var tempCourselDict = {};
+            for (i=0; i<LJdata.length; i++){
+                console.log(LJdata)
+                var LJid = LJdata[i].LJ_ID; 
+                var SkillId = LJdata[i].Skill_ID; 
+                var CourseId = LJdata[i].Course_ID; 
+                var CourseName = LJdata[i].Course_Name;
+
+                if(!tempCourselDict[CourseId]){
+                    tempCourselDict[CourseId] ={CourseName:CourseName}
+                    this.Allcourses_dict.push({LJid: LJid, SkillId:SkillId, CourseId:CourseId, CourseName:CourseName})
+                }
+            }
+            console.log(this.Allcourses_dict)
+            return this.Allcourses_dict
         }
+    }
 })
 const homepagevm = homepage.mount("#homepage");
