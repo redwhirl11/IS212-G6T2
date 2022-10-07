febaa3bd1eb31fc7ae5fad7910576dce153e2347
kwwwk <91783866+kwwwk@users.noreply.github.com>
2022-10-08 04:46:06 +0800

    course completion status are now populated dynamically

--- js/homepage.js
+++ js/homepage.js
@@ -3,14 +3,16 @@ const homepage = Vue.createApp({
         return {
             AllRoles_dict: [],
             Allcourses_dict: [],
+
         }
     },
     mounted:function()  {
-            url="../db/getLearningJourney.php"
+            //calling LJ data
+            LJurl="../db/getLearningJourney.php"
             var Staff_ID = 00000
             const data ={Staff_ID:Staff_ID}
 
-            axios.get(url, {
+            axios.get(LJurl, {
                 params: data
             })
             .then(response => { 
@@ -18,11 +20,23 @@ const homepage = Vue.createApp({
                 this.status = response.data
                 this.getLJdetails(LJdata)
                 this.getCourseDetails(LJdata);
+                
+                //calling registered course data
+                RegCourseurl="../db/getRegCourse.php"
+                axios.get(RegCourseurl,{
+                    params:data
+                })
+                .then(response=>{
+                    var RegCourse=response.data;
+                    this.getCompletionStatus(RegCourse)
+                })            
+                .catch(error => {
+                    this.status = 'There was an error: ' + error.message 
+                }) 
             })
             .catch(error => {
                 this.status = 'There was an error: ' + error.message 
             }) 
-            
         },
     methods:{
         getLJdetails(LJdata){
@@ -57,7 +71,6 @@ const homepage = Vue.createApp({
         getCourseDetails(LJdata){
             var tempCourselDict = {};
             for (i=0; i<LJdata.length; i++){
-                console.log(LJdata)
                 var LJid = LJdata[i].LJ_ID; 
                 var SkillId = LJdata[i].Skill_ID; 
                 var CourseId = LJdata[i].Course_ID; 
@@ -69,7 +82,30 @@ const homepage = Vue.createApp({
                 }
             }
             return this.Allcourses_dict
-        }
+        },
+        getCompletionStatus(RegCourse){
+            for (i=0; i<RegCourse.length; i++){
+                var Course_ID = RegCourse[i].Course_ID; 
+                var Reg_Status = RegCourse[i].Reg_Status; 
+                var Completion_Status = RegCourse[i].Completion_Status;
+
+                for(j=0; j<this.Allcourses_dict.length; j++){
+                    if(this.Allcourses_dict[j].CourseId== Course_ID){
+                        // if course not registered, remove the entry from dict
+                        if(Reg_Status !== "Registered"){
+                            this.Allcourses_dict.splice(j,1)
+                        }
+                        // as long as registered, save the completion status in respective dict
+                        else{
+                            if(Completion_Status){
+                                this.Allcourses_dict[j]['CompletionStatus']= Completion_Status
+                            }
+                        }
+                    }
+                }
+            }
+            return this.Allcourses_dict
+        },       
     }
 })
 const homepagevm = homepage.mount("#homepage");
