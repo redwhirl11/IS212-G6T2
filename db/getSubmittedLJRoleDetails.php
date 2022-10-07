e4d5b3fe09b2682f38834cc7b94ba52ddef8bd8a
fangting99 <ftneo.2020@scis.smu.edu.sg>
2022-10-07 17:22:30 +0800

    create new php to retrieve submitted courseID

--- db/getSubmittedLJRoleDetails.php
+++ db/getSubmittedLJRoleDetails.php
@@ -3,22 +3,39 @@ require_once 'common.php';
 $dao = new PostDAO();
 
 $posts=[];
+
 // need to modify -- hardcode method
-$LJ_ID = 00001;
+$LJ_ID = 00006;
 $posts = $dao->getSubmittedRoleDetails($LJ_ID);
 
 //dynamic method
-// if(isset($_REQUEST['LJ_ID'])){
-//     $LJ_ID= $_REQUEST['LJ_ID'];
-//     $posts = $dao->getSubmittedRoleDetails($LJ_ID);
+// if(isset($_REQUEST['Staff_ID'])){
+//     $Staff_ID= $_REQUEST['Staff_ID'];
+//     $posts = $dao->getSubmittedRoleDetails($Staff_ID);
 // }
+
  // Get an Indexed Array of Post objects
 $items = [];
 foreach( $posts as $post_object ) {
     $item = [];
-    $item["SubmittedLJRole_ID"] = $post_object->getSubmittedLJRole_ID();
-    $item["SubmittedSkill_ID"] = $post_object->getSubmittedSkill_ID();
-    $item["Submitted_CourseID"] = $post_object->getSubmitted_CourseID();
+    $item["LJ_ID"] = $post_object->getLJ_ID();
+    $item["Staff_ID"] = $post_object->getStaff_ID();
+    $item["LJRole_ID"] = $post_object->getLJRole_ID();
+    $item["LJRole_Name"] = $post_object->getLJRole_Name();
+    $item["LJRole_Description"] = $post_object->getLJRole_Description();
+    $item["Department"] = $post_object->getDepartment();
+    $item["Key_Task"] = $post_object->getKey_Task();
+    $item["LJRole_img"] = $post_object->getLJRole_img();
+    $item["Skill_ID"] = $post_object->getSkill_ID();
+    $item["Skill_Name"] = $post_object->getSkill_Name();
+    $item["Type_of_Skills"] = $post_object->getType_of_Skills();
+    $item["Level_of_Competencies"] = $post_object->getLevel_of_Competencies();
+    $item["Skill_img"] = $post_object->getSkill_img();
+    $item["Course_ID"] = $post_object->getCourse_ID();
+    $item["Course_Name"] = $post_object->getCourse_Name();
+    $item["Course_Desc"] = $post_object->getCourse_Desc();
+    $item["Course_Type"] = $post_object->getCourse_Type();
+    $item["Course_Category"] = $post_object->getCourse_Category();
     $items[] = $item;
 }
 // make posts into json and return json data
