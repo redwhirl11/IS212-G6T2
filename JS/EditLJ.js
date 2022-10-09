ea62004bdb5fb9d5b7f5015df7b548c4fd44030d
SuetLi1014 <suetli.lam.2020@scis.smu.edu.sg>
2022-10-09 18:00:40 +0800

    try to get the course reg status

--- JS/EditLJ.js
+++ JS/EditLJ.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({
     data() {
-        return {      
+        return {
             roleName: '',
             roleDesc: '',
             department: '',
@@ -17,41 +17,40 @@ const app = Vue.createApp({
             //AllXX_dict means retrieve from Role Details
             Allskill_dict: [],
             Allcourse_dict: [],
-            new_selected_course:[],
+            new_selected_course: [],
             //retrieve data from getLJ.php
-            SubmittedCourse_dict:[],
-            submittedCourseID:'',
-            checked: false
-            
+            SubmittedCourse_dict: [],
+            submittedCourseID: '',
+            checked: false,
+            courseRegStatus_dict: []
         };
     },
     created() {
         //modify the code -- wait for kw to pass the data LJRole_ID,LJ_ID,Staff_ID
         var LJRole_ID = 00002
         var LJ_ID = 00006
-        var Staff_ID=00002
+        var Staff_ID = 00002
 
         //relevant php file
         RoleDetailsUrl = '../db/getLJRoleDetails.php'
-        RegCourseurl='../db/getRegCourse.php'
-        LjDetailsurl='../db/getSubmittedLJRoleDetails.php'
+        RegCourseurl = '../db/getRegCourse.php'
+        LjDetailsurl = '../db/getSubmittedLJRoleDetails.php'
         const RoleDetaildata = { LJRole_ID: LJRole_ID }
         const LJDetaildata = { LJ_ID: LJ_ID }
-        const RegCourseData={ Staff_ID:Staff_ID}
+        const RegCourseData = { Staff_ID: Staff_ID }
 
         //get the default information
         this.LJ_ID = LJ_ID;
         this.Staff_ID = Staff_ID;
         this.SubmittedLJRole_ID = LJRole_ID;
 
-        
         //retrieve data from the php 
         axios.get(LjDetailsurl, {
             params: LJDetaildata
         })
             .then(response => {
                 var LJDetails = response.data;
-                console.log('LJDetails',LJDetails)
+                console.log('LJDetails', LJDetails)
                 this.getSubmittedCourse_ID(LJDetails);
             })
         axios.get(RoleDetailsUrl, {
@@ -62,21 +61,23 @@ const app = Vue.createApp({
                 this.getRoleDetails(RoleDetails);
                 this.getSkillDetails(RoleDetails);
                 this.getCourseDetails(RoleDetails);
-                this.getRegStatus();
-                
-            })   
+            })
 
-        axios.get(RegCourseurl,{
-            params:RegCourseData
+        axios.get(RegCourseurl, {
+            params: RegCourseData
         })
-        .then(response=>{
-            var RegCourse=response.data;
-        })
+            .then(response => {
+                var RegCourse = response.data;
+                // console.log('reg course', RegCourse)
+                this.getRegStatus(RegCourse)
+                console.log('course status', this.courseRegStatus_dict)
+                // this.matchStatusWithCourse()
+            })
     },
     methods: {
         getRoleDetails(RoleDetails) {
             this.roleName = RoleDetails[0].LJRole_Name
-            this.department=RoleDetails[0].Department
+            this.department = RoleDetails[0].Department
             this.roleDesc = RoleDetails[0].LJRole_Description
             var keyTasks = RoleDetails[0].Key_Task
             this.tasks = keyTasks.split('.')
@@ -93,31 +94,31 @@ const app = Vue.createApp({
                 var Level_of_Competencies = RoleDetails[i].Level_of_Competencies;
                 var Skill_img = RoleDetails[i].Skill_img;
 
-                
                 //check if the skill has already appeared in the temp dict
                 //if no, add the skill to the Allskill_dict
                 if (!temp_skill_dict[SkillID]) {
                     temp_skill_dict[SkillID] = { SkillName: SkillName, Type_of_Skills: Type_of_Skills, Level_of_Competencies: Level_of_Competencies, Skill_img: Skill_img }
 
                     //e.g. of Allskill_dict = {SkillName:Python, Type of Skills: Technical, LOC: Beginner}
-                    this.Allskill_dict.push({ Skill_ID:SkillID,SkillName: SkillName, Type_of_Skills: Type_of_Skills, Level_of_Competencies: Level_of_Competencies, Skill_img: Skill_img })
+                    this.Allskill_dict.push({ Skill_ID: SkillID, SkillName: SkillName, Type_of_Skills: Type_of_Skills, Level_of_Competencies: Level_of_Competencies, Skill_img: Skill_img })
+
                 }
             }
-            
+
             return this.Allskill_dict;
         },
-        getSubmittedCourse_ID(LJDetails){
-            for (i = 0; i < LJDetails.length; i++){
+        getSubmittedCourse_ID(LJDetails) {
+            for (i = 0; i < LJDetails.length; i++) {
                 var SubmittedC_ID = LJDetails[i].Course_ID;
-                this.SubmittedCourse_dict.push({SubmittedC_ID: SubmittedC_ID})
+                this.SubmittedCourse_dict.push({ SubmittedC_ID: SubmittedC_ID })
             }
             return this.SubmittedCourse_dict;
         },
         getCourseDetails(RoleDetails) {
             //for each skillID
-            for (var j = 0; j < this.Allskill_dict.length; j++){
+            for (var j = 0; j < this.Allskill_dict.length; j++) {
                 var Skill_ID = this.Allskill_dict[j].Skill_ID
-                
+
                 //for each skill in ljroles
                 for (i = 0; i < RoleDetails.length; i++) {
                     var SkillID = RoleDetails[i].Skill_ID;
@@ -131,22 +132,22 @@ const app = Vue.createApp({
                     // if yes == under same skill, will put the course info in one dict
                     if (Skill_ID == SkillID) {
                         var index = '';
-                        
+
                         // find the index of ljcourseid in the submittedcourse_dict
-                        index = this.SubmittedCourse_dict.map(object =>object.SubmittedC_ID).indexOf(courseID); 
+                        index = this.SubmittedCourse_dict.map(object => object.SubmittedC_ID).indexOf(courseID);
                         console.log(courseID);
                         console.log(index);
-                        
+
                         //if index == -1 means the course has not been submitted before, checkbox checked = false (not selected)
-                        if(index != -1){
-                            this.Allcourse_dict.push({ Skill_ID:SkillID, Course_ID: courseID, Course_Name: courseName, Course_Desc: courseDesc, Course_Type: coursetype, Course_Category: coursecat, checked: true})
-                        }else{
-                            this.Allcourse_dict.push({ Skill_ID:SkillID, Course_ID: courseID, Course_Name: courseName, Course_Desc: courseDesc, Course_Type: coursetype, Course_Category: coursecat, checked: this.checked})
+                        if (index != -1) {
+                            this.Allcourse_dict.push({ Skill_ID: SkillID, Course_ID: courseID, Course_Name: courseName, Course_Desc: courseDesc, Course_Type: coursetype, Course_Category: coursecat, checked: true })
+                        } else {
+                            this.Allcourse_dict.push({ Skill_ID: SkillID, Course_ID: courseID, Course_Name: courseName, Course_Desc: courseDesc, Course_Type: coursetype, Course_Category: coursecat, checked: this.checked })
                         }
                     }
                 }
             }
-            //console.log(this.Allcourse_dict);
+            console.log('allCourse_dict', this.Allcourse_dict);
             return this.Allcourse_dict;
         },
         getCoursePopUp(Course_Name, Course_Desc, Course_Type, Course_Category) {
@@ -162,14 +163,14 @@ const app = Vue.createApp({
                 confirmButtonText: 'Back!'
             })
         },
-        saveLJ(){
+        saveLJ() {
             Swal.fire({
                 title: 'Save your Learning Journey?',
                 text: "Please check information before saving!",
                 icon: "warning",
                 showCancelButton: true,
                 cancelButtonColor: '#c7c6c5',
-                confirmButtonColor: '#6A79F3',        
+                confirmButtonColor: '#6A79F3',
                 confirmButtonText: 'Yes, save it!',
                 cancelButtonText: 'No, Cancel',
                 width: 'auto',
@@ -179,36 +180,37 @@ const app = Vue.createApp({
                     this.getNewSelectedCourse();
                     console.log(this.new_selected_course);
                     //if learner select >1 courses -> confirmation pop up
-                    if(this.new_selected_course.length>0){
-                        for (let i=0; i<this.new_selected_course.length;i++){  
+                    if (this.new_selected_course.length > 0) {
+                        for (let i = 0; i < this.new_selected_course.length; i++) {
                             //save the latest selected courses to the database
                             const Surl = '../db/saveEditedLJ.php'
-                            var Submitted_Skill_ID = parseInt(this.new_selected_course[i].skill)    
-                            var Submitted_CourseID = this.new_selected_course[i].selectedCourse                       
-                            const data = { LJ_ID: this.LJ_ID, Staff_ID: this.Staff_ID,
-                                            SubmittedLJRole_ID: this.SubmittedLJRole_ID, 
-                                            Submitted_Skill_ID:Submitted_Skill_ID,
-                                            Submitted_CourseID:Submitted_CourseID,
-                                            }
+                            var Submitted_Skill_ID = parseInt(this.new_selected_course[i].skill)
+                            var Submitted_CourseID = this.new_selected_course[i].selectedCourse
+                            const data = {
+                                LJ_ID: this.LJ_ID, Staff_ID: this.Staff_ID,
+                                SubmittedLJRole_ID: this.SubmittedLJRole_ID,
+                                Submitted_Skill_ID: Submitted_Skill_ID,
+                                Submitted_CourseID: Submitted_CourseID,
+                            }
                             axios.get(Surl, {
                                 params: data
                             })
-                                
+
                                 .then(response => {
                                     // console.log(response);
                                     Swal.fire(
-                                            'Congratulations!',
-                                            'Your changes have been successfully saved!',
-                                            'success',
-                                        )                                
-                                    })
-                                        
+                                        'Congratulations!',
+                                        'Your changes have been successfully saved!',
+                                        'success',
+                                    )
+                                })
+
                                 .catch(error => {
                                     console.log(error);
                                     alert('Error: ${error}. <br/> Please Try Again Later')
-                                })                       
+                                })
                         }
-                    }else{
+                    } else {
                         Swal.fire(
                             'Cancelled',
                             'Oops, you must pick a least 1 course :(',
@@ -218,28 +220,33 @@ const app = Vue.createApp({
                 }
             })
         },
-        getNewSelectedCourse(){
+        getNewSelectedCourse() {
             //reset the new_selected_course
-            this.new_selected_course=[];
+            this.new_selected_course = [];
 
             //find the latest selected courses by learner
-            for (var i = 0; i < this.Allcourse_dict.length; i++){
+            for (var i = 0; i < this.Allcourse_dict.length; i++) {
                 var selectedCourse = this.Allcourse_dict[i].Course_ID
                 var selected = this.Allcourse_dict[i].checked
                 var skill = this.Allcourse_dict[i].Skill_ID
-                
+
                 //if checkbox is checked(selected by the learner), add the skillID, courseID to the new_selected_course list
-                if (selected==true){
-                    this.new_selected_course.push({skill,selectedCourse})
+                if (selected == true) {
+                    this.new_selected_course.push({ skill, selectedCourse })
                 }
             }
             return this.new_selected_course;
         },
-        
 
-        getRegStatus(){
+        getRegStatus(RegCourse) {
+            for (i = 0; i < RegCourse.length; i++) {
+                var courseID = RegCourse[i].Course_ID;
+                var completionStatus = RegCourse[i].Completion_Status;
+                this.courseRegStatus_dict.push({ course_ID: courseID, completion_Status: completionStatus })
+            }
 
-        }
+            return this.courseRegStatus_dict
+        },
 
     }
 })
