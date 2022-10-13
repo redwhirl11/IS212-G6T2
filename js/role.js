fe04c23b9de0993452a8d5ad85948a7804cfa33e
jingwenn <43838212+jingwenn@users.noreply.github.com>
2022-10-13 21:21:19 +0800

    Update role.js to display alert message when db is empty

--- js/role.js
+++ js/role.js
@@ -3,27 +3,31 @@ function getAllRoles(){
     axios.get(url).
     then(response => {
         console.log(response.data)
-        const key = 'LJRole_Name';
-        const arrayUniqueByKey = [...new Map(response.data.map(item =>[item[key], item])).values()];    
-        console.log('here',arrayUniqueByKey)
-        var card = '';
-        for (i=0; i<arrayUniqueByKey.length; i++) {
-          console.log(arrayUniqueByKey[i])
-          card += 
-          `<div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card">
-              <img class="card-img-top" src=" ${arrayUniqueByKey[i].LJRole_img} " alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title"> ${arrayUniqueByKey[i].LJRole_Name} </h5>
-                  <p class="card-text">${arrayUniqueByKey[i].Key_Task}</p>
-                  <a href="#" class="btn btn-primary" id="styled" onClick="populateHeader(${i})">View More</a>
-                </div>
-            </div>
-          </div>`
+
+        if (response.data.length == 0) {
+          alert('No results found')
+        } else {
+          const key = 'LJRole_Name';
+          const arrayUniqueByKey = [...new Map(response.data.map(item =>[item[key], item])).values()];    
+          console.log('here',arrayUniqueByKey)
+          var card = '';
+          for (i=0; i<arrayUniqueByKey.length; i++) {
+            console.log(arrayUniqueByKey[i])
+            card += 
+            `<div class="col-lg-4 col-md-4 col-sm-6">
+              <div class="card">
+                <img class="card-img-top" src=" ${arrayUniqueByKey[i].LJRole_img} " alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title"> ${arrayUniqueByKey[i].LJRole_Name} </h5>
+                    <p class="card-text">${arrayUniqueByKey[i].Key_Task}</p>
+                    <a href="#" class="btn btn-primary" id="styled" onClick="populateHeader(${i})">View More</a>
+                  </div>
+              </div>
+            </div>`
+          }
+
+          document.getElementsByClassName('row')[0].innerHTML = card 
         }
-
-        document.getElementsByClassName('row')[0].innerHTML = card 
-
     })
     .catch(error => {
         console.log(error.message)
