1d7372f8f967cf50471eb02b711fc2c9e5179a26
redwhirl11 <earnestho.2020@scis.smu.edu.sg>
2022-10-13 09:03:36 +0800

    edits on getlatestLJ_ID.php

--- db/getLatestLJ_ID.php
+++ db/getLatestLJ_ID.php
@@ -9,8 +9,8 @@ $posts = $dao->getLJs();
  // Get an Indexed Array of Post objects
  $items = [];
  foreach( $posts as $post_object ) {
-     $items[] = $post_object->getLJ_ID();
+     $items[] = intval($post_object->getLJ_ID());
  }
  // make posts into json and return json data
- echo $items[count($items) - 1];
+ echo max($items);
  ?>
