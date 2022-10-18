6879d15205d8af17940fa791e1ba5f57b1a67c2f
SuetLi1014 <suetli.lam.2020@scis.smu.edu.sg>
2022-10-18 16:54:06 +0800

    display the role details dynamically in hrRole page

--- JS/hrRole.js
+++ JS/hrRole.js
@@ -7,18 +7,40 @@ app = Vue.createApp({
 
     created() {
         this.getAllRole()
-        console.log('testing')
+        // console.log('testing')
     },
 
     methods: {
+        
         getAllRole() {
-            allRoleUrl = "../db/getAllRoles.php"
-
+            const allRoleUrl = '../db/getAllRoles.php'
             axios.get(allRoleUrl).then(response => {
                 var allRole = response.data
                 console.log(allRole)
-            })
 
+                var temp=[];
+
+                for (i=0;i<allRole.length;i++){
+                    var roleId=allRole[i].LJRole_ID
+                    var roleName= allRole[i].LJRole_Name
+                    var dept=allRole[i].Department
+                    var desc=allRole[i].LJRole_Description
+                    var task=allRole[i].Key_Task
+                    var status=allRole[i].LJRole_Status
+                    var skill=allRole[i].Skill_ID
+
+                    if (!temp[roleId]){
+                        temp[roleId]={roleName:roleName,dept:dept,desc:desc,task:task,status:status,skill:skill}
+
+                        this.roleDict.push({roleId:roleId,roleName:roleName,dept:dept,desc:desc,task:task,status:status,skill:skill})
+                    }
+                   
+                }
+                console.log('temp', temp)
+            })
+            
+            console.log(this.roleDict)
+            return this.roleDict;
         }
     }
 })
