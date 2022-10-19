c70db3184ee9d987e939e73dc79bbb252caf40cb
jingwenn <43838212+jingwenn@users.noreply.github.com>
2022-10-19 15:35:28 +0800

    Edit to pass roleID to softdeleterole function

--- JS/hrRole.js
+++ JS/hrRole.js
@@ -41,6 +41,38 @@ app = Vue.createApp({
             
             console.log(this.roleDict)
             return this.roleDict;
+        },
+
+        SoftDeleteRole(id) {
+            console.log(id)
+
+            Swal.fire({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to see this role!",
+                icon: "warning",
+                cancelButtonColor: '#c7c6c5',
+                cancelButtonText: 'Cancel',
+                confirmButtonText: 'Yes',
+                showCancelButton: true,
+        
+              })
+              .then((result) => {
+                if (result.isConfirmed) {
+                    var url = "../db/SoftDeleteRole.php"
+                    axios.get(url).
+                    then(response => {
+                        console.log(response.data)
+                        Swal.fire("Success! Role has been soft deleted.", {
+                        icon: "success",
+                        });
+                    })
+                    .catch(error => {
+                        console.log(error.message)
+                    })
+                } else {
+                    Swal.fire("Role has not been soft deleted!");
+                }
+              });
         }
     }
 })
