7c44b9ec9cf01ae49fbd2b1027ea5efc108666a9
kwwwk <91783866+kwwwk@users.noreply.github.com>
2022-11-06 20:41:10 +0800

    updated unit test cases

--- src/components/__tests__/hrAssignCoursetoSkill.spec.js
+++ src/components/__tests__/hrAssignCoursetoSkill.spec.js
@@ -5,10 +5,11 @@ import hrCreateSkill from "../hrCreateSkill.vue";
 
 //Course_assign is null
 describe("hrCreateSkill", () => {
-  it("renders properly", () => {
+  it("Null Value for Course_assign", () => {
     const wrapper = mount(hrCreateSkill,{
       propsData: {
-        Course_assign:'',
+        Type_of_Skill:'smth',
+        Course_assign:false,
       }   
     })
     //result from the function
@@ -20,20 +21,21 @@ describe("hrCreateSkill", () => {
   });
 });
 
-//skill name is null
+// Course_assign field is not empty
 describe("hrCreateSkill", () => {
-    it("renders properly", () => {
-      const wrapper = mount(hrCreateSkill,{
-        propsData: {
-            Skill_Name:'',
-        }   
-      })
-            
-      //result from the function
-      wrapper.vm.getErrorMessage()
-      
-      //expected result
-      expect(wrapper.vm.errorm).toBe('Invalid Skill Name');
-    
-    });
+  it("Assign 1 course to the skill", () => {
+    const wrapper = mount(hrCreateSkill,{
+      propsData: {
+        Skill_Name:'Communication',
+        Type_of_Skill:'General',
+        Level_of_Competencies:'Beginner',
+        Course_assign:true,
+      }   
+    })   
+    //result from the function
+    wrapper.vm.getErrorMessage()    
+    //expected result
+    expect(wrapper.vm.errorm).toBe('');
+  
   });
+});
