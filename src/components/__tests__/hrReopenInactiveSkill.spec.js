cae72535f7ce400e2d3959fe847568a21f8548cd
jingwenn <43838212+jingwenn@users.noreply.github.com>
2022-11-08 09:49:44 +0800

    G6T2-37 Updated build.yml and Created a js file for tdd

---
+++ src/components/__tests__/hrReopenInactiveSkill.spec.js
@@ -0,0 +1,25 @@
+// import { describe, it, expect } from "vitest";
+
+// import { mount } from "@vue/test-utils";
+// import hrCreateRole from "../hrCreateRole.vue";
+
+// describe("hrCreateRole", () => {
+//   it("renders properly", () => {
+//     const wrapper = mount(hrCreateRole,{
+//       propsData: {
+//         Department: 'ceo',
+//         RoleName:'',
+//         skills_required:'00007',
+//         // error: 'Invalid Role Name'
+//       }
+    
+//     })
+//     // const byId = wrapper.find('#submitBTN')
+//     // //CLICK id submitBTN
+//     // expect(byId.element.id).toBe('submitBTN')    
+//     //can call for data(parameter)
+//     wrapper.vm.getErrorMessage()
+//     expect(wrapper.vm.errorm).toBe('Invalid Role Name');
+  
+//   });
+// });
